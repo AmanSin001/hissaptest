71e8fcbec8c9f97b0c2f6bf9663c3f9b314a4043
Aman Singh <amanmsingh23@gmail.com>
2023-01-18 04:12:31 +0530

    new

--- android/app/src/main/java/com/hissap/MainApplication.java
+++ android/app/src/main/java/com/hissap/MainApplication.java
@@ -30,7 +30,6 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new SplashScreenReactPackage());
-          packages.add(new RNCameraPackage());
           return packages;
         }
 
